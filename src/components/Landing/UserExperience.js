fad09f4410d2742f223168acd3bb634a7f21b0d6
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-04-03 01:26:18 -0400

    Video link

--- src/components/Landing/UserExperience.js
+++ src/components/Landing/UserExperience.js
@@ -5,7 +5,7 @@ const UserExperience = (props) => {
         <div ref={props.getRef} id='ux' className='uxView'>
             <h1>User Experience</h1>
             <div className='videoView leftRightPadding'>
-                <p>Video will insert in this card view</p>
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/mVqg_x1G7ms" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     )
